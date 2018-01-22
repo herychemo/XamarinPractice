c9bf44bff7670e38b5811c1e5bd328fe0cd132ef
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2018-01-21 23:22:17 -0600

    ,

---
+++ SharedListsApp/SharedListsApp_BLL/WebClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Http;
+
+namespace SharedListsApp
+{
+    public class WebClient
+    {
+
+        private HttpClient httpClient;
+        public RestClient(HttpMessageHandler handler)
+        {
+            httpClient = new HttpClient(handler);
+        }
+    }
+}
